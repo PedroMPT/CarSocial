540a2b67d599987f20d01189c110a0071e2f48ba
Pedro Almeida <A032107@ismai.pt>
2018-12-30 23:58:30 +0000

    List of Ride Layout Implementation_v2

--- app/src/main/java/pt/ismai/pedro/needarideapp/Activities/ListOfRidesActivity.java
+++ app/src/main/java/pt/ismai/pedro/needarideapp/Activities/ListOfRidesActivity.java
@@ -1,32 +1,117 @@
 package pt.ismai.pedro.needarideapp.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pt.ismai.pedro.needarideapp.Adapters.RecyclerViewAdapter;
 import pt.ismai.pedro.needarideapp.R;
 
 public class ListOfRidesActivity extends AppCompatActivity {
 
+    final ArrayList<Bitmap> avatar = new ArrayList<>();
+    final ArrayList<String> avatarName = new ArrayList<>();
+    final ArrayList<String> price = new ArrayList<>();
+    final ArrayList<String> time = new ArrayList<>();
+    final ArrayList<String> rideDate = new ArrayList<>();
+    final ArrayList<String> rideFrom = new ArrayList<>();
+    final ArrayList<String> rideTo = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_rides);
 
-
+        initRideInfo();
         initiateRecyclerView();
 
     }
 
-    private void initiateRecyclerView(){
+    private void initRideInfo(){
 
-        //RecyclerView recyclerView = findViewById(R.id.mt_recycler);
-        //RecyclerViewAdapter adapter = new RecyclerViewAdapter(this,images,context);
-        //recyclerView.setAdapter(adapter);
-        //recyclerView.setLayoutManager( new LinearLayoutManager(this));
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Ride");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> rides, ParseException e) {
+
+                if ( e == null){
+
+                    for (ParseObject ride : rides){
+
+                        price.add((String) ride.get("price"));
+                        time.add((String) ride.get("time"));
+                        rideDate.add((String) ride.get("data"));
+                        rideFrom.add((String) ride.get("from"));
+                        rideTo.add((String) ride.get("to"));
+
+                    }
+                }
+
+            }
+        });
+
+        ParseObject ride = new ParseObject("Ride");
+
+        ParseQuery<ParseUser> queryU = ParseUser.getQuery();
+        queryU.whereEqualTo("objectId",ride.get("user_id"));
+
+        queryU.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                if (e == null){
+
+                    for (ParseUser user: users){
+
+                        String firstName = (String) user.get("name");
+                        String lastName = (String) user.get("lat_name");
+                        avatarName.add(firstName + " " + lastName);
+
+                        ParseFile file = (ParseFile) user.get("profile_photo");
+
+                        file.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+
+                                Bitmap bitmap = BitmapFactory.decodeByteArray(data,0,data.length);
+                                avatar.add(bitmap);
+                            }
+                        });
+
+                    }
+                }
+            }
+        });
 
 
     }
+
+
+
+    private void initiateRecyclerView(){
+
+        RecyclerView recyclerView = findViewById(R.id.mt_recycler);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(getApplicationContext(),avatar,avatarName,price,time,rideDate,rideFrom,rideTo);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager( new LinearLayoutManager(this));
+
+
+    }
+
+
 }
--- app/src/main/java/pt/ismai/pedro/needarideapp/Adapters/RecyclerViewAdapter.java
+++ app/src/main/java/pt/ismai/pedro/needarideapp/Adapters/RecyclerViewAdapter.java
@@ -1,30 +1,46 @@
 package pt.ismai.pedro.needarideapp.Adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import pt.ismai.pedro.needarideapp.R;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
 
     // Definir as Variáveis do conteúdo
-    private ArrayList<String> images;
-    private Context context;
+    private ArrayList<Bitmap> avatar;
+    private ArrayList<String> avatarName;
+    private ArrayList<String> price;
+    private ArrayList<String> time;
+    private ArrayList<String> rideDate;
+    private ArrayList<String> rideFrom;
+    private ArrayList<String> rideTo;
+    private LayoutInflater inflater;
 
     // Criar o construtor
 
 
-    public RecyclerViewAdapter(Context context, ArrayList<String> images) {
-        this.images = images;
-        this.context = context;
+    public RecyclerViewAdapter(Context context, ArrayList<Bitmap> avatar, ArrayList<String> avatarName, ArrayList<String> price, ArrayList<String> time, ArrayList<String> rideDate, ArrayList<String> rideFrom, ArrayList<String> rideTo) {
+        inflater = LayoutInflater.from(context);
+        this.avatar = avatar;
+        this.avatarName = avatarName;
+        this.price = price;
+        this.time = time;
+        this.rideDate = rideDate;
+        this.rideFrom = rideFrom;
+        this.rideTo = rideTo;
     }
 
     @NonNull
@@ -33,32 +49,51 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         // Inflate the view
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_folding,viewGroup,false);
-        ViewHolder viewHolder = new ViewHolder(view);
-        return null;
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
 
-
+        viewHolder.title_price.setText(price.get(i));
+        viewHolder.title_date.setText(rideDate.get(i));
+        viewHolder.title_time.setText(time.get(i));
+        viewHolder.title_from_address.setText(rideFrom.get(i));
+        viewHolder.title_to_address.setText(rideTo.get(i));
+        viewHolder.avatar_name.setText(avatarName.get(i));
+        viewHolder.avatar.setImageBitmap(avatar.get(i));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return price.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
-
-        RelativeLayout parentLayout;
+        LinearLayout linearTitleLayout;
+        RelativeLayout leftSideRelativeLayout,rightTitleRelativeLayout;
+        TextView title_price,title_time,title_date,avatar_name,title_from_address,title_to_address;
+        CircleImageView avatar;
 
 
         public ViewHolder(@NonNull View itemView) {
 
            super(itemView);
 
-            //parentLayout = itemView.findViewById(R.id.parent_layout);
+           linearTitleLayout = itemView.findViewById(R.id.linearTitleLayout);
+           leftSideRelativeLayout = itemView.findViewById(R.id.leftSideRelativeLayout);
+           rightTitleRelativeLayout = itemView.findViewById(R.id.rightTitleRelativeLayout);
+           title_price = itemView.findViewById(R.id.title_price);
+           title_time = itemView.findViewById(R.id.title_time_label);
+           title_date = itemView.findViewById(R.id.title_date_label);
+           avatar_name = itemView.findViewById(R.id.avatar_name);
+           title_from_address = itemView.findViewById(R.id.title_from_address);
+           title_to_address = itemView.findViewById(R.id.title_to_address);
+           avatar = itemView.findViewById(R.id.avatar);
+
+
         }
     }
+
 }
